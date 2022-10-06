e4104846f050db58c5c2da5c42be3c8fe2922e1b
Daryyya <dara9841402670@gmail.com>
2022-10-06 18:57:30 +0300

    test

--- script.js
+++ script.js
@@ -29,6 +29,9 @@ $wrapBtn.prepend($btnDelChecked, $btnDelAll);
 //
 
 let $id = '';
+let arr = [];
+
+
 
 function createCheckBox ($value) {
 
@@ -48,17 +51,17 @@ function createCheckBox ($value) {
 
     $label.prepend($checkInput, $checkSpan)
 
-        $label.addEventListener('click', addCheckedValue);
-        function addCheckedValue () {
-            if ($checkInput.checked === 'true') {
-                $label.style.textDecoration = 'none';
+        // $label.addEventListener('click', addCheckedValue);
+        // function addCheckedValue () {
+        //     if ($checkInput.checked === 'true') {
+        //         $label.style.textDecoration = 'none';
 
-            }
-            else {
-            $label.style.textDecoration = 'line-through';
-            $label.style.opacity = '0.5';
-            }
-        }
+        //     }
+        //     else {
+        //     $label.style.textDecoration = 'line-through';
+        //     $label.style.opacity = '0.5';
+        //     }
+        // }
 
     let $delCheckBox = document.createElement('div');
         $delCheckBox.textContent = '❌';
@@ -72,7 +75,21 @@ function createCheckBox ($value) {
     
         $wrapCheckBox.prepend($label, $delCheckBox);
 
+        function createObj ($value, $id) {
+            let obj = {
+                value: `${$value}`,
+                id: `${$id}`,
+            }
+            return obj;
+        }
+
+        arr.push(createObj($value, $id));
+
+        console.log(arr);
+
+
         return $wrapCheckBox;
+        
 }
 
 
@@ -83,9 +100,13 @@ function createCheckBoxList () {
     if ($input.value) {
         $wrapper.style.minHeight = '283px';
 
+        $id += 'a';
+
         $wrapCheckBoxList.prepend(createCheckBox($input.value));
 
         $wrapper.append($wrapCheckBoxList, $wrapBtn);
+
+        localStorage.setItem('checkboxes', JSON.stringify(arr));
     }
 
     else {
@@ -100,4 +121,69 @@ function createCheckBoxList () {
         $input.style.border = '1px solid #CED4DA';
     } 
 }
+
+// const url = 'http://localhost:3000'
+
+
+
+// let test = fetch(url + '/getsomedata').then((res) => res.json()).then((data) => console.log(data.message))
+
+// let aaaa = {
+//     arr: [1, 2, 3]
+// }
+
+// async function ttt () {
+//     let response = await fetch(url + '/test', {
+//         method:"POST", 
+//         headers: {
+//             "Content-type": "application/json"
+//         },
+//         body: JSON.stringify(arr)})
+//     let data = await response.json()
+//     alert(data.mes)
+// }
+
+// ttt();
+// const btnReg = document.querySelector('.reg');
+
+// const popup = document.createElement('div');
+// popup.className = 'popup';
+// document.body.prepend(popup);
+
+// const modal = document.createElement('div');
+// modal.className = 'modal';
+// popup.prepend(modal);
+
+// btnReg.addEventListener('click', () => alert('hello'));
+
+const url = 'https://virtserver.swaggerhub.com/a-berezhkov/todo_app_sc_bc/1.0.0/rest-user';
+
+const formTest = document.querySelector('.formTest');
+const btnTest = document.querySelector('.btnTest');
+
+const input1 = document.getElementById('1');
+const input2 = document.getElementById('2');
+const input3 = document.getElementById('3');
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+let response = fetch(url).then((res) => res.json()).then((data) => console.log(data));
+
+
+
+
+
+
+
 
