25e38799dba3eae5f438091a38705da377a8462a
Daryyya <darya9841402670@gmail.com>
2022-10-11 17:44:16 +0300

    del

--- script.js
+++ script.js
@@ -1,117 +1,13 @@
-// // common wrapper for todo list
-// const wrapper = document.querySelector('.wrapper');
-// // 
-// const wrapCheckBoxList = document.createElement('div')
-// // input type text 
-// const input = document.querySelector('.text');
-// // btn for add task
-// const btnAddCheckBox = document.querySelector('.btn');
-// // 
-// wrapBtn = document.querySelector('.wrapBtn');
-
-
-
-// wrapper.append(wrapCheckBoxList);
-
-// function createElement (status, value) {
-//     let wrapCheckBox = document.createElement('div');
-//         wrapCheckBox.className = 'wrapCheckBox';
-
-//     let label = document.createElement('label');
-//         label.className = 'option';
-//         label.className = 'check';
-//         label.textContent = value;
-    
-//     let checkInput = document.createElement('input');
-//         checkInput.className = 'check__input';
-//         checkInput.type = 'checkbox';
-    
-//     let checkSpan = document.createElement('span');
-//         checkSpan.className = 'check__box';
-    
-//         label.prepend(checkInput, checkSpan);
-    
-//     let delCheckBox = document.createElement('div');
-//         delCheckBox.textContent = '❌';
-
-//         checkInput.checked = status;
-
-//         if (status) {
-//             label.classList.add('.line');
-//         }
-//         else {
-//             label.classList.remove('.line');
-//         }
-
-//         wrapCheckBox.append(label, delCheckBox);
-
-//         wrapCheckBoxList.addEventListener('click', () => {
-//             let value = event.target;
-//             console.log(value);
-//             if (value.type == "checkbox") {
-//                 label.toggleAttribute("line")
-//                 // check.nextSibling.classList.toggle("checked")
-//                 saveTasks()
-//             } else { 
-//                 // обработка нажатия на крестик
-//                 // label.outerHTML = null;
-//         }}
-//         )
-
-//         return wrapCheckBox;
-// }
-
-// function renderElement (status, value) {
-//     wrapCheckBoxList.append(createElement(status, value));
-// }
-
-// function saveElement () {
-//     let arr = [];
-//     const list = document.querySelectorAll('.check');
-//     list.forEach(el => {
-//         arr.push({
-//             status : el.childNodes[0].checked,
-//             value: el.innerText,
-//         })
-//     });
-
-//     localStorage.setItem('list', JSON.stringify(arr));
-//     return Object.values(JSON.parse(localStorage.getItem('list'))).length;
-// };
-
-// (function main () {
-//     if (localStorage.getItem('list')) {
-//         let arr = JSON.parse(localStorage.getItem('list'));
-//         arr.forEach(el => renderElement(el.status, el.value));
-//         wrapBtn.style.display = 'block';
-//     }
-// })()
-
-
-
-// btnAddCheckBox.addEventListener('click', () => {
-//     renderElement(true, input.value);
-//     wrapBtn.style.display = 'block';
-//     input.value = null;
-//     saveElement();
-
-//     let checkIsEmpty = saveElement();
-//     if(!checkIsEmpty) {
-//         wrapBtn.style.display = 'none';
-//         localStorage.clear();
-//     }
-// })
-
-
-
-
+// common wrapper
 const $wrapper = document.querySelector('.wrapper');
+// input type text
 const $input = document.querySelector('.text');
+// btn add task
 const $btnAddCheckBox = document.querySelector('.btn');
+// wrapper for checkbox list
 const $wrapCheckBoxList = document.createElement('div'); 
 $wrapCheckBoxList.className =  'wrapCheckBoxList';
-
-// 
+// block: button wrapper
 
 const $wrapBtn = document.createElement('div');
 $wrapBtn.className = 'wrapBtn';
@@ -119,7 +15,6 @@ $wrapBtn.className = 'wrapBtn';
 const $btnDelChecked = document.createElement('button');
 $btnDelChecked.className = 'btnDelChecked';
 $btnDelChecked.textContent = 'Удалить завершенные задачи';
-
 
 const $btnDelAll = document.createElement('button');
 $btnDelAll.className = 'btnDelAll';
@@ -202,23 +97,8 @@ function createCheckBox ($value) {
         return $wrapCheckBox;
         
 };
-
-(function main () {
-    if(localStorage.getItem('checkboxes')) {
-        let arr = JSON.parse(localStorage.getItem('checkboxes'));
-
-        arr.forEach(el => $wrapCheckBoxList.prepend(createCheckBox(el.value)))
-        $wrapper.append($wrapCheckBoxList, $wrapBtn)
-        
-    }
-})()
-
-
-$btnAddCheckBox.addEventListener('click', () => createCheckBoxList());
-
 function createCheckBoxList () {
     if ($input.value) {
-        // $wrapper.style.minHeight = '283px';
 
         $id += 'a';
 
@@ -241,6 +121,21 @@ function createCheckBoxList () {
         $input.style.border = '1px solid #CED4DA';
     } 
 }
+
+(function main () {
+    if(localStorage.getItem('checkboxes')) {
+        let arr = JSON.parse(localStorage.getItem('checkboxes'));
+
+        arr.forEach(el => $wrapCheckBoxList.prepend(createCheckBox(el.value)))
+        $wrapper.append($wrapCheckBoxList, $wrapBtn)
+        
+    }
+})()
+
+
+$btnAddCheckBox.addEventListener('click', () => createCheckBoxList());
+
+
 
 
 
